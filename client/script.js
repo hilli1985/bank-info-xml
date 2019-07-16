ab5d110c614ac1b71234bd9e0491a4642aa6f18d
Hilla Meir <hillameir85@gmail.com>
2019-07-17 00:45:40 +0300

    add autocomplete

--- client/script.js
+++ client/script.js
@@ -23,54 +23,100 @@ const infoKeys = {
     // Y_Coordinate:'lan',
 }
 
+// variables
+let input = document.getElementById('input');
+//let people = ['john doe', 'maria', 'paul', 'george', 'jimmy'];
+let people = [];
+
+
+function myFunc(str) {
+    console.log(str)
+    alert('You clicked me');
+}
+
+// functions
+function autocomplete(val) {
+    let people_return = [];
+
+    for (let i = 0; i < people.length; i++) {
+        if (val === people[i].slice(0, val.length)) {
+            people_return.push(people[i]);
+        }
+    }
+
+    return people_return;
+}
+
+const li = document.createElement('li');
+li.innerText = "client";
+li.id = "key";
+li.className = "client-class";
+li.setAttribute("onclick", "valid;");
+
+
+
+// events
+input.onkeyup = async function (e) {
+    people = await getInput(e);
+    console.log(people)
+    let input_val = this.value; // updates the variable on each ocurrence
+    if (input_val.length === 0) {
+        let autocomplete_results = document.getElementById("autocomplete-results");
+        autocomplete_results.innerHTML = '';
+    }
+    else if (input_val.length > 0) {
+        let people_to_show = [];
+
+        let autocomplete_results = document.getElementById("autocomplete-results");
+        autocomplete_results.innerHTML = '';
+        people_to_show = autocomplete(input_val);
+
+        for (let i = 0; i < people_to_show.length; i++) {
+            autocomplete_results.innerHTML += `<li class="list-element" onclick="myFunc( ${i})" > ${people_to_show[i]['Bank_Name']}</li>`;
+        }
+        autocomplete_results.style.display = 'block';
+    } else {
+        people_to_show = [];
+        autocomplete_results.innerHTML = '';
+    }
+}
+
 async function clickHandler() {
     const response = await getBranchInfo(13, branchCodeInput.value);
     let str = '';
-    str = `<h3 class="h-bank-info">${response.data['Branch_Name']._text} (${response.data['Branch_Code']._text})</h3>`; 
-    str = str + `<h4 class="h-bank-info">${response.data['Bank_Name']._text}</h4>`; 
+    str = `<h3 class="h-bank-info">${response.data['Branch_Name']._text} (${response.data['Branch_Code']._text})</h3>`;
+    str = str + `<h4 class="h-bank-info">${response.data['Bank_Name']._text}</h4>`;
     str = str + Object.keys(infoKeys).map(key => {
-        if (key === 'Branch_Name'|| key ==='Branch_Code' || key ==='Bank_Name') {
-            return ``        
+        if (key === 'Branch_Name' || key === 'Branch_Code' || key === 'Bank_Name') {
+            return ``
         }
         else {
             return `<div><div class="h-field-bank-info" >${infoKeys[key]}</div>: ${response.data[key]._text}</div>`
         }
     }).join('')
-brachInfo.outerHTML = `<div id="bankInfo" class="col-sm-8 bank-info">${str}</div>`;
+    brachInfo.outerHTML = `<div id="bankInfo" class="col-sm-8 bank-info">${str}</div>`;
 }
 
-function selectBranch() {
-    alert('You clicked me');
-}
+
 
 let timeout = null;
 
-function createAutoComplete (autocompleteArr) {
-    return autocompleteArr.data.map (a => `<option id="autoComplete">auto auto hi hi</option>`).join('')  
-}
-
-const showDropdown = function (element) {
-    var event = document.createEvent('MouseEvents');
-    event.initMouseEvent('mousedown', true, true, window);
-    element.dispatchEvent(event);
-    element.autoFocus = true
-};
-
-showDropdown(bankNameInput);
 
 async function getInput(e) {
     e.preventDefault()
     clearTimeout(timeout);
+    let autocompleteArr = [];
     timeout = setTimeout(async function () {
         console.log('Input Value:', e.target.value);
-        const autocompleteArr = await getAutocomplete(e.target.value);
-        console.log('autocompleteArr', autocompleteArr)
-        console.log(createAutoComplete(autocompleteArr))
-        bankNameInput.outerHTML = `<select id="bankNameInput">${createAutoComplete(autocompleteArr)}<select>`;
-        const branchList = await getBranchList(13)
-        console.log('branchList', branchList)
+        autocompleteArr = await getAutocomplete(e.target.value);
+        console.log('autocompleteArr', autocompleteArr.data)
+        // return autocompleteArr;
+        // people = autocompleteArr;
+        // const branchList = await getBranchList(13)
+        // console.log('branchList', branchList)
     }, 2000);
 };
 
 document.getElementById('btnSubmit').onclick = clickHandler;
-document.getElementById('bankNameInput').onkeyup = getInput;
+// document.getElementById('input').onkeyup = getInput;
+window.myFunc = myFunc
